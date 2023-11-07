be2feb37c7f847155b057a2a0de41b9f5de35f08
SofiaaaaL <yl8586@nyu.edu>
2023-11-07 09:37:33 -0500

    commit on nov 7th

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
+const firebase = require("firebase/app");
+//Port
 const port = 4000;
 
 // app.get("/", (req, res) => {
 //     res.send("Hello World")
 // });
 
+//the unique project configuration info for firebaase
+const firebaseConfig = {
+    apiKey: "AIzaSyBX4Dg0ppUX-Dul8aQrMmM_qMNWIAbhsN8",
+    authDomain: "exercise-five-fall-2023-sl.firebaseapp.com",
+    projectId: "exercise-five-fall-2023-sl",
+    storageBucket: "exercise-five-fall-2023-sl.appspot.com",
+    messagingSenderId: "925125857734",
+    appId: "1:925125857734:web:29d99f488c1fa01a3022c7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const indexRoute = require('./routes/index');
 // const singlePostRoute = require('./routes/singlePost');
 // const createPostRoute = require('./routes/createPost');
+
 
 app.use("/", indexRoute)
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const router = express.Router();
+//Initializee firestore database
+const firestore = require("firebase/firestore");
+//create a reference to the database
+const db = firestore.getFirestore();
 
-// router.get("/", (req, res) => {
-//     res.send("All Posts!");
-// });
-
-const SAMPLE_API_RESPONSE = [
-    
-]
+//define index routes
+router.get("/", (req, res) => {
+    //getting the collection post
+    const postsQuery = firestore.getDocs(firestore.collection(db, "Posts"));
+    //defining an empty array
+    const postsArray = [];
+    //looping the data and putting it into the array
+    postsQuery
+        .then((response) => {
+            response.forEach((post) => {
+                console.log(post.data());
+                postsArray.push({ id: post.id, ...post.data() }); //... spreads out the values inside of the array; instead of having the whole array inside of the array
+            });
+            res.send(postsArray);
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.send(error);
+        });
+});
 
 module.exports = router
